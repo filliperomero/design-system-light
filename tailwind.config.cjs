a92490836722c04df2a0d4786117fbe73496232d
filliperomero <fillipe.romero@gmail.com>
2022-10-15 12:52:32 -0600

    add colours token to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,19 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
+      },
+      colors: {
+        gray: {
+          900: '#121214',
+          800: '#202024',
+          400: '#7C7C8A',
+          200: '#E1E1E6',
+          100: '#CACACC',
+        },
+        cyan: {
+          500: '#81D8F7',
+          300: '#9BE1FB'
+        }
       }
     },
   },
